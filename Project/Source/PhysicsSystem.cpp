78760714f556e9a6a0a4c24faf6df2f169289ee1
ViktorLjung <ljunga_93@hotmail.com>
2015-05-31 21:44:54 +0200

    fix fix

--- Project/Source/PhysicsSystem.cpp
+++ Project/Source/PhysicsSystem.cpp
@@ -88,7 +88,7 @@ void PhysicsSystem::Update(double dt)
 			trans = object->getWorldTransform();
 		}
 		//printf(" world   pos  object  %d = %f ,%f ,%f\n", j, float(trans.getOrigin().getX()), float(
-			trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+		//	trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
 	}
 
 }
